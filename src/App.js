e502c83412c81024cea7b706c8aa570b70a791ea
Pol Reig <polreigbroto@gmail.com>
2021-06-10 20:34:52 +0200

    botones activados y contador en marcha

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const increment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
+
+  const limitNumber = (counter) => {
+    counter = Math.min(100, Math.max(0, counter));
+  };
   return (
     <>
       <div className="container">
-        <button className="boton" type="submit">
-          <i className="icono far fa-plus"></i>
+        <button onClick={decrement} className="boton" sign="-">
+          <i className="icono">➖</i>
         </button>
-        <input className="input-number" type="number"></input>
-        <button className="boton" type="submit">
-          <i className="icono far fa-minus"></i>
+
+        <input
+          className="input-number"
+          id="num"
+          value={counter}
+          type="number"
+          min="0"
+          max="10"
+        />
+
+        <button onClick={increment} className="boton" sign="+">
+          <i className="icono">➕</i>
         </button>
       </div>
     </>
