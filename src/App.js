e72c838a0fd8dc363112c96f4e8c98f32764d37d
Pol Reig <polreigbroto@gmail.com>
2021-06-10 19:12:34 +0200

    Esqueleto HTML y estilos CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
 function App() {
   return (
-
-  )
+    <>
+      <div className="container">
+        <button className="boton" type="submit">
+          <i className="icono far fa-plus"></i>
+        </button>
+        <input className="input-number" type="number"></input>
+        <button className="boton" type="submit">
+          <i className="icono far fa-minus"></i>
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
